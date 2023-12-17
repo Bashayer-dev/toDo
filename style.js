60f67ac6deeb6887bc21379b613754763c971548
Bashayer <62783833+BashayerAlHarbi@users.noreply.github.com>
2023-12-17 13:32:49 +0300

    Update style.js

--- style.js
+++ style.js
@@ -1,19 +1,26 @@
+// Selecting DOM Elements
 const title = document.getElementById("title");
+const input = document.getElementById("todo-input");
+const btn = document.getElementById("add-btn");
+const list = document.getElementById("todo-list");
+const label = document.getElementById("label");
+const search = document.getElementById("search");
 
 // Set the date to the title
 const date = new Date().toString().slice(0, 15);
 title.textContent = `Today is ${date}`;
 
 // Create Button
-function createButton(type, color) {
+function createButton(type, color, value) {
   // create
   const button = document.createElement("button");
   // add class
   button.classList.add(`${type.toLowerCase()}-btn`);
   // add text button
-  button.innerHTML = `${type}`;
+  button.textContent = `${type}`;
   // add color
   button.style.backgroundColor = `${color}`;
+  button.setAttribute("value", `${value}`);
   // STYLING
   button.style.height = "2rem";
   button.style.width = "5rem";
@@ -25,6 +32,15 @@ function createButton(type, color) {
   return button;
 }
 
+// Create checkbox
+function checkbox() {
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  // checkbox.setAttribute("id", "check");
+  checkbox.style.margin = "10px";
+  return checkbox;
+}
+
 // Style input field
 function required(str, color, cursor) {
   label.textContent = str;
@@ -33,4 +49,15 @@ function required(str, color, cursor) {
   btn.style.cursor = cursor;
 }
 
-export { createButton, required, title, date };
+export {
+  createButton,
+  checkbox,
+  required,
+  title,
+  input,
+  btn,
+  list,
+  label,
+  search,
+  date,
+};
